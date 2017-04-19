ef0bdde72488815a3a7ec35544ce0ecb58153402
Tasuka Hasegawa <tasuka923@gmail.com>
2017-04-19 07:30:12 +0000

    post

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,12 @@
 class ToppagesController < ApplicationController
+  
+  include SessionsHelper
+  
   def index
+    if logged_in?
+      @user = current_user
+      @micropost = current_user.microposts.build  # form_for 用
+      @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
+    end
   end
 end
