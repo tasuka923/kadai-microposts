3b5af17b5886c0ef8eab6bcb247025377de22d36
Tasuka Hasegawa <tasuka923@gmail.com>
2017-04-20 08:25:33 +0000

    10.2 router

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -7,6 +7,7 @@ class ToppagesController < ApplicationController
       @user = current_user
       @micropost = current_user.microposts.build  # form_for 用
       @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
+      redirect_to root_path
     end
   end
 end
